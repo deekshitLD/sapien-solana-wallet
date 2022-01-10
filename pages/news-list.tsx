774682ce1f244d0ab562d46bc80363d53e3a22d3
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2022-01-10 22:09:05 +0530

    fixed styles

--- pages/news-list.tsx
+++ pages/news-list.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import Home from "../src/components/home/index";
 
 const HomePage: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Home />
     </div>
   );
