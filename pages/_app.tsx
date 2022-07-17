a50da3f3ed04567863b1c803a93f9130137a6f6d
deekshitLD <deekshitdsouza@gmail.com>
2022-07-18 00:03:53 +0530

    Page Path added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,7 +40,9 @@ const App: FC<AppProps> = ({ Component, pageProps }: any) => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-              gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
+              page_path: window.location.pathname,
+            });
          `}
     </Script>
     <WalletConnectionProvider>
