4a1cbc9cd9ee26ad37a2d393d3b5aa8ad2807fee
deekshitLD <deekshitdsouza@gmail.com>
2022-07-18 00:12:22 +0530

    Page Path removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,9 +40,7 @@ const App: FC<AppProps> = ({ Component, pageProps }: any) => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
-              page_path: window.location.pathname,
-            });
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
          `}
     </Script>
     <WalletConnectionProvider>
