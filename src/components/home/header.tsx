eb0dfed183d8dcf58a902faf6acadf1d4712d29b
Abhinav Sharma <abhinavsharma2689@gmail.com>
2022-07-23 20:42:57 +0530

    Crypto msg

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -25,7 +25,7 @@ const Header: React.FC = () => {
           paddingX={['4px', '12px', '12px', '16px', '24px']}
           paddingY={['4px', '4px', '12px', '12px', '16px']}
         >
-        Decentralized News
+          Crypto news,<br/> minus the noise
         </Heading>
         <Heading
           as="h3"
@@ -37,7 +37,7 @@ const Header: React.FC = () => {
           paddingX={['24px', '24px', '30px', '30px', '30px']}
           paddingY={['8px', '8px', '14px', '14px', '14px']}
         >
-          Crypto news - minus the noise
+          Decentralized Platform
         </Heading>
       </Flex>
     </Grid>
