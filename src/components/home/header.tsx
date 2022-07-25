3e6c9213b6cf6a2656fccf160b0b95ddc86cafda
Abhinav Sharma <abhinavsharma2689@gmail.com>
2022-07-25 19:10:09 +0530

    cleaner

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -11,35 +11,6 @@ const Header: React.FC = () => {
       height={['0', '0', '0', '0']}
       templateRows="1fr 1fr 1fr"
     >
-      {/* <Flex
-        flexDirection="column"
-        gridRow="3 / 3"
-        paddingX={['1.5em', '1.5em', '1.5em', '15%']}
-      >
-        <Heading
-          as="h2"
-          fontSize={['3xl', '4xl', '5xl', '5xl', '5xl']}
-          color="white"
-          mb="3"
-          width="fit-content"
-          paddingX={['4px', '12px', '12px', '16px', '24px']}
-          paddingY={['4px', '4px', '12px', '12px', '12px']}
-        >
-          Decentralized News
-        </Heading>
-        <Heading
-          as="h3"
-          fontSize={['lg', 'lg', '2xl', '2xl', '2xl']}
-          color="black"
-          bg="white"
-          width="fit-content"
-          paddingX={['24px', '24px', '30px', '30px', '30px']}
-          paddingY={['8px', '8px', '14px', '14px', '14px']}
-          margin={['20px']}
-        >
-          Crypto for Humans
-        </Heading>
-      </Flex> */}
     </Grid>
   )
 }
