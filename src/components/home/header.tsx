cbdfdd16b98b61e6fe4a1001bae6c98028f08fbd
Abhinav Sharma <abhinavsharma2689@gmail.com>
2022-07-25 19:42:05 +0530

    flex changes

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -8,9 +8,36 @@ const Header: React.FC = () => {
       backgroundPosition="center"
       backgroundRepeat="no-repeat"
       backgroundSize="cover"
-      height={['0', '0', '0', '0']}
+      height={['0px']}
+      width={['0px']}
       templateRows="1fr 1fr 1fr"
     >
+      <Flex
+        flexDirection="column"
+        gridRow="3 / 3"
+        paddingX={['1.5em', '1.5em', '1.5em', '15%']}
+      >
+        <Heading
+          as="h2"
+          fontSize={['3xl', '4xl', '5xl', '5xl', '5xl']}
+          color="white"
+          mb="3"
+          width="fit-content"
+          paddingX={['4px', '12px', '12px', '16px', '24px']}
+          paddingY={['4px', '4px', '12px', '12px', '12px']}
+        >
+        </Heading>
+        <Heading
+          as="h3"
+          fontSize={['lg', 'lg', '2xl', '2xl', '2xl']}
+          color="black"
+          width="fit-content"
+          paddingX={['24px', '24px', '30px', '30px', '30px']}
+          paddingY={['8px', '8px', '14px', '14px', '14px']}
+          margin={['20px']}
+        >
+        </Heading>
+      </Flex>
     </Grid>
   )
 }
