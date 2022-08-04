51da4b5eb0766ddffab79a8a44b9197035ade8a8
Abhinav Sharma <abhinavsharma2689@gmail.com>
2022-08-04 08:27:39 +0530

    margin

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC = () => {
       backgroundPosition="center"
       backgroundRepeat="no-repeat"
       backgroundSize="cover"
-      height={['59vh']}
+      height={['43vh']}
       width={['auto']}
       templateRows="1fr 1fr 1fr"
     >
@@ -38,7 +38,7 @@ const Header: React.FC = () => {
           width="fit-content"
           paddingX={['24px', '24px', '30px', '30px', '30px']}
           paddingY={['8px', '8px', '14px', '14px', '14px']}
-          margin={['0','0','15vh','0']}
+          margin={['0','0','20','0']}
         >
           Join the waitlist
         </Heading>
