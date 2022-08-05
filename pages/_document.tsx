04133873ed7479358f989e37498a768d56d1fc32
Abhinav Sharma <abhinavsharma2689@gmail.com>
2022-08-05 21:08:47 +0530

    fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            href="https://fonts.googleapis.com/css?family=Space_Grotesk:300,400,500,700&display=swap"
           />
         </Head>
         <body>
