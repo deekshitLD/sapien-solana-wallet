38d80e9c37550a5cb8a1cff2ecc1356b69f5df87
deekshitLD <deekshitdsouza@gmail.com>
2022-08-06 00:02:22 +0530

    pagination route commented on index for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ import {
 } from "@solana/spl-token";
 import Header from "../src/components/home/header";
 import Footer from "../src/components/home/footer";
-import Pagination from "../src/components/home/Pagination";
+//import Pagination from "../src/components/home/Pagination";
 
 import Home from "../src/components/home";
 
@@ -99,9 +99,9 @@ const HomePage: any = () => {
           </>
 
           {/* Pagination */}
-          <>
+          {/* <>
             <Pagination />
-          </>
+          </> */}
       </main>
       <Footer />
       <div></div>
